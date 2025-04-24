9f8a2f4fed78043dee1a213afa1d4073405bf811
Cris Messias <meunomeecriss@gmail.com>
2025-04-24 08:45:54 +0200

    feat(implement-ai): change the iOS targate to 17, add tests

--- GeoRisques/GeoRisquesTests/GeoRisquesTests.swift
+++ GeoRisques/GeoRisquesTests/GeoRisquesTests.swift
@@ -6,13 +6,13 @@ struct GeoRisquesTests {
     func viewDidLoad() async throws {
         let store = GeoRisquesStore(
             locationClient: FixedLocationClient(location: .france),
-            risquesClient: FixedRisquesClient(risques: Risque.diseases),
+            risquesClient: FixedRisquesClient(risques: Risque.naturalDisasters),
         )
         store.onRisquesDidLoad()
         
         await megaYield()
         #expect(store.risquesState.location == .france)
-        #expect(store.risquesState.risques == Risque.diseases)
+        #expect(store.risquesState.risques == Risque.naturalDisasters)
     }
     
     @Test("on location button tap, moves map to current location and reload risks")
@@ -34,13 +34,21 @@ struct GeoRisquesTests {
             locationClient: FixedLocationClient(location: .grenoble),
             risquesClient: FixedRisquesClient(risques: Risque.all),
         )
-        store.onRisqueButtonTapped(.dengue)
-        #expect(store.risquesState.selectedRisque == GeoRisquesStore.RisqueDetailState(risque: .dengue))
+        store.onRisqueButtonTapped(.affaissementMinier)
+        #expect(store.risquesState.selectedRisque == GeoRisquesStore.RisqueDetailState(risque: .affaissementMinier))
     }
     
     private func megaYield() async {
         for _ in 0..<100 {
             await Task.yield()
         }
+    }
+
+    @Test("on fetch advice, show AI answer")
+    func fetchAdivice() async throws {
+        let language = "en_EN"
+        let advice = try await OpenAIClientLive().askAI(prompt: "Écriz moi quelquer chose en \(language)?")
+        print(advice)
+        #expect(!advice.isEmpty)
     }
 }
